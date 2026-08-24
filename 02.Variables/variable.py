7c187c195c9fd61b1d4d55c6222fc66abfdc2613
dhyani-patel-CG <dhyani.patel.cg@gmail.com>
2026-08-24 10:04:37 +0530

    file done

---
+++ 02.Variables/variable.py
@@ -0,0 +1,36 @@
+#que = 24
+name = "rahul"
+age = 22
+city = "ahemdabad"
+
+#que = 25
+student_name = "rahul"
+student_roll_number = 22
+student_branch = "CSE"
+
+#que = 26
+marks = 88
+print(marks)
+marks = 98
+print(marks)
+
+#que = 27
+name , city , age = "rahul" , "ahemdabad" , 22
+
+#que = 28
+x = y = z = 7
+
+#que = 29
+student_name = "rahul"
+student_full_name = "rahul"
+student_class = "B.tech"
+
+#que = 30
+# store and update a student's information
+
+student_name = "rahul"
+student_age = 23
+student_age = 24
+print(f"student_name: {student_name}")
+print(f"student_age: {student_age}")
+

31070d2c0a4c2f0ba977600bb78c07ea50249f25
dhyani-patel-CG <dhyani.patel.cg@gmail.com>
2026-08-26 00:12:58 +0530

    file updated

---
+++ 04.Arithmatic-oprations/arithmatic.py
@@ -0,0 +1,203 @@
+# Arithmatic oprations
+# part 2 , que 1
+print(10 + 5)
+print(10 - 5)
+print(10 * 5)
+print(10 / 5)
+print(10 // 3)
+print(10 % 3)
+print(2 ** 4)
+
+# que 2
+print(10 + 5 * 2)
+print((10 + 5) * 2)
+
+# que 3
+print(20 / 5 * 2)
+print(20 // 3 * 2)
+print(20 - 5 + 2)
+
+# que 4
+print(10 - (-5))
+print(-10 - 5)
+print(-10 - (-5))
+
+# que 5
+print(10 // 3)
+print(-10 // 3)
+print(10 // -3)
+print(-10 // -3)
+
+# que 6
+print(10 % 3)
+print(-10 % 3)
+print(10 % -3)
+print(-10 % -3)
+
+# que 7
+print(-2 ** 2)
+print((-2) ** 2)
+print(-2 ** 3)
+print((-2) ** 3)
+
+# que 8
+print(True + True)
+print(True + False)
+print(False * 10)
+print(True * 10)
+print(True / 2)
+
+# que 9
+print("Python" + "Programming")
+print("Python" * 3)
+
+
+# part 3 , task 1
+a = 4
+b = 6
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a*b)
+
+#task 2
+a = 5
+b = 6.9
+print(type(a+b) , a+b)
+print(type(a-b) , a-b)
+print(type(a/b) , a/b)
+print(type(a//b) , a//b)
+print(type(a*b) , a*b)
+print(type(a**b) , a**b)
+
+#task 3
+
+marks1 = 89
+marks2 = 95
+print(marks1+marks2)
+print((marks1+marks2)/2)
+
+#task 4
+
+product_price = 500
+Quantity = 2
+print(product_price+Quantity)
+
+#task 5
+
+a = 5
+b = 2
+number%2=0
+
+#task 6
+
+a = 5
+b = 4
+print(a/b)
+print(a//b)
+
+#tast 7
+
+a = -4
+b = -7
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+
+#tast 8
+
+a = -7
+b = 5
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+
+#tast 9
+
+a = 7
+b = 9
+print(a//b)
+print((-a)//b)
+print(a//(-b))
+print((-a)//(-b))
+
+#tast 10
+
+print(5%7)
+print((-5)%7)
+print(5%(-7))
+print((-5)%(-7))
+
+#part task 11
+
+print(10 + 5 * 2)
+print(20 - 4 / 2)
+print(10 + 20 / 5 * 2)
+print(2 + 3 * 4 ** 2)
+print(100 - 20 // 5)
+
+#task 12
+
+print(10 + 5 * 2)
+print((10 + 5) * 2)
+
+print(20 - 10 / 2)
+print((20 - 10) / 2)
+
+print(2 + 3 * 4)
+print((2 + 3) * 4)
+
+#task 13
+
+a = True
+b = False
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+
+#task 14
+
+print(True + 5)
+print(False + 5)
+print(True * 10)
+print(False * 10)
+print(True - 5)
+print(False - 5)
+
+#task 15
+
+First_name = "Dhyani"
+Last_name = "Patel"
+print(First_name+Last_name)
+
+#tast 16
+
+a = "dhyani"
+b = 3
+print(a**b)
+
+#tast 17
+
+a = "6"
+b = "7"
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+
+#task 18
+
+value = None
+print

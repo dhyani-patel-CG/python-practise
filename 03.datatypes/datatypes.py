f638af86ae33b2a11b5833f67f9789a41afd8eb4
dhyani-patel-CG <dhyani.patel.cg@gmail.com>
2026-08-25 10:11:38 +0530

    file updated

---
+++ 03.datatypes/datatypes.py
@@ -0,0 +1,82 @@
+Age = 22
+age = "22"
+aGe = 22.2
+AGE = None
+agE = True
+print(type(Age))
+print(type(age))
+print(type(aGe))
+print(type(agE))
+print(type(AGE))
+
+#que 24
+
+student_name = "Rahul"
+student_age = 20
+student_height = 25.5
+is_the_person_a_student = True
+student_result = None
+
+#que 25
+
+a = 50
+b = 50.0
+c = "50"
+print(type(a))
+print(type(b))
+print(type(c))
+
+#que 26
+
+a = True
+b = "True"
+print(type(a)) #because it is a boolean datatypes
+print(type(b)) #beacuse it is a string datatypes
+
+#que 27
+
+a = None
+b = "None"
+print(type(a)) #because it is a none datatype
+print(type(b)) #because it it a string datatype
+
+#que 28
+
+value = 50
+print(type(value))
+value1 = str(value)
+print(type(value1)) #value's datatype is int and value1's datatype is str
+
+#que 29 (info about product)
+
+product_name = "bad"
+product_quantity = 1
+product_price = 550.49
+product_availbilty = True
+product_discount = None
+print(type(product_name))
+print(type(product_quantity))
+print(type(product_price))
+print(type(product_availbilty))
+print(type(product_discount))
+
+#que 30
+
+age = 10
+Age = 10.0
+aGe = "10"
+possibal = True
+Possible = "True"
+availbilty = None
+Availbilty = "None"
+print(type(age))
+print(type(Age))
+print(type(aGe))
+print(type(possibal))
+print(type(Possible))
+print(type(availbilty))
+print(type(Availbilty))
+
+
+
+

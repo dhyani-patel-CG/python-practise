fc47df9d53425e87488b607662d16c77196f2a42
dhyani-patel-CG <dhyani.patel.cg@gmail.com>
2026-08-26 23:02:07 +0530

    file updated

--- 05.python-string-opration/stringopraters.py
+++ 05.python-string-opration/stringopraters.py
@@ -1,52 +1,99 @@
 
-#1
-
-name="dhyani"
-city="modasa"
-language="gujrati"
-mass="hii"
-print(name,city,language,mass)
-
-#2
-
-a="tararata"
-
+#creating a string
+name="Dhyani"
+city_name='Modasa'
+fav_programming_lang="""Python"""
+message="hellooo everyone"
+print(name)
+print(city_name)
+print(fav_programming_lang)
+print(message)
+#empty string
+a=""
 print(a)
 print(len(a))
 print(type(a))
-
-#3
-
-b="Python Programming"
-
-print(b[0:],len(b),b[0],b[18],b[17])
-
-#4
-
-c="Programming"
-
-print(c[0],c[1],c[4],c[9])
-
-#5
-
-print(c[-1],c[-2],c[-3],c[-11])
-
-#6
-
-d="tarang"
-
-print(d[0],d[5])
-
-#7
-
-print(b[0:6],b[7:],b,b[:5],b[12:])
-
-#8
-
-e="ABCDEFGHIJKL"
-
-print(e[1:8:2])
-
-#9
-
-print(e[1:8:-1])
+#string information
+a="Python Programming"
+print(a[:])
+print(len(a))
+print(a[0])
+print(a[-1])
+print(a[2])
+print(a[-2])
+#positive indexing
+a="Programming"
+print(a[0])
+print(a[1])
+print(a[4])
+print(a[-1])
+#negative indexing
+a="Programming"
+print(a[-1])
+print(a[-2])
+print(a[-3])
+print(a[-11])
+#indexing challenge
+a="Dhyani patel"
+print(a[0])
+print(a[-1])
+print(a[6])
+#basic slicing
+a="Python Programming"
+print(a[0:6])
+print(a[7:17])
+print(a[:])
+print(a[:5])
+print(a[-5:])
+#slicing with step
+a="ABCDEFGHIJKL"
+print(a[::2])
+print(a[::3])
+print(a[1:8:2])
+print(a[::-1])
+#slicing with negative indexes
+a="Python Programming"
+print(a[-5:])
+print(a[-10:])
+print(a[::-1])
+#slicing challenge
+a="Python Programming"
+print(a[:4])
+print(a[-3:])
+print(a[::2])
+print(a[::-1])
+print(a[1:-2])
+#length
+a="hello"
+b="hello world"
+c="hello how are you all "
+print(len(a))
+print(len(b))
+print(len(c))
+#task 12
+text = "Python Programming"
+print(len(text)-1)
+#concatenation
+first_name="Dhyani"
+last_name="Patel"
+print(first_name + ' ' + last_name)
+#sentence creation
+name="Dhyani"
+age=19
+age=str(age)
+city_name="Modasa"
+fav_programming_lang="Python"
+print(name + " " + age + " " + city_name + " " + fav_programming_lang)
+#string and integer
+a=19
+a=str(a)
+b="age"
+print(a+ " " +b)
+#string repetation
+a="@"
+print(a*3)
+print(a*5)
+print(a*10)
+#pattern
+a="*" * 10
+print(a)

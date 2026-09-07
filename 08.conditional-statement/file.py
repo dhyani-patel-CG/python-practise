91941762107fa595728a6878bc110764536eb020
dhyani-patel-CG <dhyani.patel.cg@gmail.com>
2026-09-07 09:33:31 +0530

    file updated

---
+++ 08.conditional-statement/file.py
@@ -0,0 +1,49 @@
+#result=30
+#if result>32 :
+#    print("pass")
+
+#age = int(input("enter your age: ").split()[0])
+#gender = input("enter your gender: ")
+#gender = gender.lower().strip()
+#print(age , gender)
+
+#if age>=18:
+ #   if gender=="female":
+  #      print("seat is available for you")
+
+    #if gender!="female":
+    #    print("seat is not avalable for you")
+
+   # if gender=="male":
+    #    print("book a seat")
+
+    #if gender!="male":
+    #    print("seat booking is not nessasary")     
+#age= 15
+#if age >=18:
+ #   print("you can watch this movie")   
+#else:
+ #   print("you can not watch this movie")
+
+username=input("enter username: ").strip()
+password=input("enter 5 digit password: ")[:6]
+print(username , password)
+
+if username:
+    print("enter password")
+    if password:
+        print("click submit")
+
+    else:
+        print("use another password")
+
+
+else:
+    print("use another username")    
+
+
+print("18"==18)
+print("18">=18)
+
+
+

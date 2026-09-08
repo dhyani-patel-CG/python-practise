db09106f3bc79b3147795773cb1f71194c4db220
dhyani-patel-CG <dhyani.patel.cg@gmail.com>
2026-09-08 09:30:56 +0530

    assignment done

--- 08.conditional-statement/file.py
+++ 08.conditional-statement/file.py
@@ -1,7 +1,8 @@
+# task 1
 #result=30
 #if result>32 :
-#    print("pass")
-
+ #   print("pass")
+# task2
 #age = int(input("enter your age: ").split()[0])
 #gender = input("enter your gender: ")
 #gender = gender.lower().strip()
@@ -11,39 +12,60 @@
  #   if gender=="female":
   #      print("seat is available for you")
 
-    #if gender!="female":
+   # if gender!="female":
     #    print("seat is not avalable for you")
 
-   # if gender=="male":
-    #    print("book a seat")
+    #if gender=="male":
+     #   print("book a seat")
 
     #if gender!="male":
-    #    print("seat booking is not nessasary")     
+     #   print("seat booking is not nessasary")  
+#  task 3
 #age= 15
 #if age >=18:
  #   print("you can watch this movie")   
 #else:
  #   print("you can not watch this movie")
 
-username=input("enter username: ").strip()
-password=input("enter 5 digit password: ")[:6]
-print(username , password)
+# task 4
+#username=input("enter username: ").strip()
+#password=input("enter 5 digit password: ")[:6]
+#print(username , password)
 
-if username:
-    print("enter password")
-    if password:
-        print("click submit")
+#if username:
+ #   print("enter password")
+  #  if password:
+   #     print("click submit")
 
-    else:
-        print("use another password")
-
-
-else:
-    print("use another username")    
+    #else:
+     #   print("use another password")
 
 
-print("18"==18)
-print("18">=18)
+#else:
+ #   print("use another username")
+
+# task 5
+# a = int(input("enter a number 1 : ")[:1])
+# b = int(input("enter a number 2 : ")[:1])
+
+# opration=int(input("1.addition /n 2.substation /n 3.multipication /n 4.devision /n 5.floor division : "))
+
+
+# if opration==1:
+#     print(a+b)
+# elif opration==2:
+#      print(a-b)
+# elif opration==3:
+#      print(a*b)
+# elif opration==4:
+#      print(a/b)
+# elif opration==5:
+#      print(a//b)
+# else:
+#      print("invalid opration")
+
+
+
 
 
 
